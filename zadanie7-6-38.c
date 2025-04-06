9bfb8833faf0b2d047ba15681ce1d9faca5f7ae0
DariaBozh <darling.bozh12326@gmail.com>
2025-04-06 19:06:52 +0300

    Libraries and a function for random character generation were added.

--- zadanie7-6-38.c
+++ zadanie7-6-38.c
@@ -1,4 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define ALPHANUM "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+#define ALNUM_LEN 62
+
+//funkcia generuje nahodny alfanumericky znak vacsi alebo rovny min
+char nahodny_znak (char min){
+	char c;
+	do {
+		c = ALPHANUM[rand() % ALNUM_LEN];
+	} while (c < min);
+	return c;
+}
 
 int main(){
 	
