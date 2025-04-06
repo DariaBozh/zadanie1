0b7630cf5fa3d96b5ba0b8e8832344373d136f6b
DariaBozh <darling.bozh12326@gmail.com>
2025-04-06 19:29:10 +0300

    Function for rewriting characters was added.

--- zadanie7-6-38.c
+++ zadanie7-6-38.c
@@ -6,13 +6,22 @@
 #define ALPHANUM "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 #define ALNUM_LEN 62
 
-//funkcia generuje nahodny alfanumericky znak vacsi alebo rovny min
+// funkcia generuje nahodny alfanumericky znak vacsi alebo rovny min
 char nahodny_znak (char min){
 	char c;
 	do {
 		c = ALPHANUM[rand() % ALNUM_LEN];
 	} while (c < min);
 	return c;
+}
+
+// funkcia prepisuje str nahodnym rastucim retazcem rovnakej dlzky
+void nahodny_rastuci_retazec(char *str){
+	char min = '0'; //majmensi povoleny znak
+	for(int i = 0; str[i]; i++){
+		str[i] = nahodny_znak(min);
+		min = str[i]; //aktualizacia minimumu
+	}
 }
 
 int main(){
